227c9e726e6dea92100bfe01f441aa4b089ee551
Nataliya Khamitsevich <nataliyakham@gmail.com>
2023-03-23 16:35:15 +0300

    hw10 preloader, redux

--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,19 +2,22 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // пишет студент  // need to fix
-
-        default:
-            return state
-    }
-}
-
 type LoadingActionType = {
     type: 'CHANGE_LOADING'
     isLoading: boolean
 }
+
+export type LoadingStateType = typeof initState
+
+export const loadingReducer = (state: LoadingStateType = initState, action: LoadingActionType): LoadingStateType => {
+    switch (action.type) {
+        case 'CHANGE_LOADING': {
+            return {...state, isLoading: action.isLoading}
+        }
+
+        default:
+            return state
+}}
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
     type: 'CHANGE_LOADING',
